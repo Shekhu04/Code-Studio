56aa7cf41f63b4e95b230e41090d8b9d8fd8de1b
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-01-02 21:13:59 +0530

    Create LookAndSaySequence.cpp

---
+++ LookAndSaySequence.cpp
@@ -0,0 +1,31 @@
+string lookAndSaySequence(int n) 
+{
+    // Write your code here.
+    if(n==1) return "1";
+    string s = lookAndSaySequence(n-1);
+
+    string ans = "";
+
+    int frequency=0;
+
+ 
+
+    for(int i=0;i<s.length();i++){
+
+        frequency++;
+
+        //separating the numbers into groups
+
+        if(i==s.length()-1 || s[i]!=s[i+1]){
+
+            ans = ans + to_string(frequency) + s[i];  //to_string type casting
+
+            frequency=0;
+
+        }
+
+    }
+
+    return ans;
+
+}
