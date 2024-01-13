55b89f3a4691fe9904f3805a0f4b7df3bd904bec
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-01-13 23:34:26 +0530

    Create LongestCommonPrefix.cpp

---
+++ LongestCommonPrefix.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    // Write your code here
+    string ans="";
+
+sort(arr.begin(),arr.end());
+
+string first = arr[0],last=arr[n-1];
+
+for(int i=0;i<min(first.size(),last.size());++i){
+
+if(first[i]!=last[i]){
+
+return ans;
+
+}else{
+
+ans+=first[i];
+
+}
+
+}return ans;
+}
+
+
